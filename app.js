fb5a442d54f14188dbeb775ce1a39ede3b3e89bb
jasminejeane <jasminejeane@gmail.com>
2018-01-03 21:51:04 -0800

    error handling

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 const https = require('https');
 // const username = 'chalkers';
+
+
+// print error messages
+
+function printError(error){
+  printError(error);
+}
+
 function printMessage(username, badgeCount, point){
   const  message =`${username} has ${badgeCount} total badges(s) and ${point} points in JavaScript`;
   console.log(message);
@@ -16,24 +24,33 @@ try{
 const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
   // console.log(response.statusCode);
   // Read data
+
+  if(response.statusCode === 200){
   let body = "";
   response.on('data', data => {
     body += data.toString();
   });
   response.on('end', () => {
     // Parse data
+    try{
     const profile = JSON.parse(body);
     // console.dir(profile);
     printMessage(username, profile.badges.length, profile.points.JavaScript);
+  }catch (error){
+    printError(error);
+  }
   });
 
-
+}else{
+  const message = `There was an error getting the profile for ${username} ({$response.statusCode})`;
+  const statusCodeError = new Error();
+}
   // Print data
 });
 
-request.on('error', error => console.error(`Problem with request:  ${error.message}`));
+request.on('error', error => printError);
 } catch(error){
-  console.error(error.message);
+  printError(error);
 }
 }
 const users = process.argv.slice(2);
